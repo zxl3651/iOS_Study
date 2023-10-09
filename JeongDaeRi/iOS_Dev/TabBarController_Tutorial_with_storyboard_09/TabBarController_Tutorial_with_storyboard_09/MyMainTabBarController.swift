30ece16292b19c1bdda4869731cd8d8d3937812d
zxl3651 <zxl3651@naver.com>
2023-10-09 19:39:10 +0900

    탭바 컨트롤러 사용해보기 - with storyboard

---
+++ JeongDaeRi/iOS_Dev/TabBarController_Tutorial_with_storyboard_09/TabBarController_Tutorial_with_storyboard_09/MyMainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarController_Tutorial_with_storyboard_09
+//
+//  Created by 이성현 on 2023/10/09.
+//
+
+import UIKit
+
+class MyMainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
