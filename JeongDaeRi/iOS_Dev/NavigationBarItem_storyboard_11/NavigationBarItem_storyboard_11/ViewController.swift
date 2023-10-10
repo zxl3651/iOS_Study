1fa74aba7a13f98c33355d575cbd7e59a3203e50
zxl3651 <zxl3651@naver.com>
2023-10-10 21:31:41 +0900

    네비게이션 바 아이템 - storyboard

---
+++ JeongDaeRi/iOS_Dev/NavigationBarItem_storyboard_11/NavigationBarItem_storyboard_11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationBarItem_storyboard_11
+//
+//  Created by 이성현 on 2023/10/10.
+//
+
+import UIKit
+
+class ViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
