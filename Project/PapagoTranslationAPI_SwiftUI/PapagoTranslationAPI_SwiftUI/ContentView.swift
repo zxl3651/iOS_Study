97a7dc30437303a42800eab5c9528d154e224dbf
zxl3651 <zxl3651@naver.com>
2023-10-18 14:22:16 +0900

    Project 폴더 생성

---
+++ Project/PapagoTranslationAPI_SwiftUI/PapagoTranslationAPI_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PapagoTranslationAPI_SwiftUI
+//
+//  Created by 이성현 on 2023/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project/PapagoTranslationAPI_SwiftUI/PapagoTranslationAPI_SwiftUI/PapagoTranslationAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapagoTranslationAPI_SwiftUIApp.swift
+//  PapagoTranslationAPI_SwiftUI
+//
+//  Created by 이성현 on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct PapagoTranslationAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
