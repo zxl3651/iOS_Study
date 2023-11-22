05a62e0ee4c55cd95291174347df892c8e0e5192
zxl3651 <zxl3651@naver.com>
2023-11-22 16:46:16 +0900

    [Study] CoreData 예제 앱 시작 - 엔티티 설명 추가, 초기 StoryBoard 설정

---
+++ LikeLion/11.22(수)/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by 이성현 on 2023/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var name : UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var status: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func saveContact(_ sender: Any) {
+    }
+    
+    @IBAction func findContact(_ sender: Any) {
+    }
+}
+
